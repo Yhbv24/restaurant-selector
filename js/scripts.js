a02a62af7a9ee4fceedc25ff80a6e9c95c8f6a7b
[mkelley2] <[m_kelley2@yahoo.com]>
2017-02-02 10:20:13 -0800

    fix bug that prevented allRest array from resetting when Home was clicked

--- js/scripts.js
+++ js/scripts.js
@@ -100,6 +100,7 @@ var nuestraCocina = new RestMaker(false, "Mexican", "full-bar", "fancy", 3, "Nue
 
 $(function(){
   $("#home").click(function() {
+    allRestaurants=[];
     $(".form_content").show();
     $(".output_content").hide();
     document.getElementById("user_input_form").reset();
